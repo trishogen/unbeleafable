ec448a768c519faf15a561c7463873675c474d8a
trishogen <patriciaogen@gmail.com>
2020-06-26 15:01:37 -0400

    redirect on logout

--- client/src/containers/navBarContainer.js
+++ client/src/containers/navBarContainer.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { logout } from '../actions/AuthActions'
 import NavBar from '../components/NavBar';
+import History from '../history'
+
 
 
 class NavBarContainer extends Component {
 
+  handleLogout = () => {
+    this.props.logout();
+    History.push('/');
+  }
+
   render() {
     return (
       <div>
-        <NavBar loggedIn={this.props.loggedIn} logout={this.props.logout}/>
+        <NavBar loggedIn={this.props.loggedIn} logout={this.handleLogout}/>
       </div>
     );
   }
