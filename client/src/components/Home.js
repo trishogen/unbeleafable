6ffb4b43d4df76c7b46a21cfa54244e1c31e8a8e
trishogen <patriciaogen@gmail.com>
2020-07-29 16:53:12 -0400

    sign up from home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 
-const Home = ({ groupArr, onDelete }) => {
+const Home = () => {
 
   return (
-    <div>
+    <div className="v-center">
       <h2> Welcome to Unbeleafable </h2>
       <p>A community of house plant lovers, gardeners, and other plant
-      enthusiasts! </p>
+      enthusiasts! <br /> Sign up to discuss all your plant related matter with
+      people who love plants as much as you do.</p>
+      <Link to="/signup">
+        <Button variant="outline-secondary" size="sm">Sign up</Button>
+      </Link>
     </div>
   )
 }
